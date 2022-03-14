2992da374abe787319f02a72e15d04f5f341d165
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-14 09:02:21 +0530

    Create dbfunc.py

---
+++ TeamIndia/Database/dbfunc.py
@@ -0,0 +1,202 @@
+from TeamIndia.Database.mongo import client as db
+from typing import Dict, List, Union
+
+
+coupledb = db.couple
+karmadb = db.karma
+gbansdb = db.gban
+chatsdb = db.chats
+
+
+# Couple Chooser
+
+async def _get_lovers(chat_id: int):
+    lovers = coupledb.find_one({"chat_id": chat_id})
+    if lovers:
+        lovers = lovers["couple"]
+    else:
+        lovers = {}
+    return lovers
+
+
+async def get_couple(chat_id: int, date: str):
+    lovers = await _get_lovers(chat_id)
+    if date in lovers:
+        return lovers[date]
+    else:
+        return False
+
+
+async def save_couple(chat_id: int, date: str, couple: dict):
+    lovers = await _get_lovers(chat_id)
+    lovers[date] = couple
+    coupledb.update_one(
+        {"chat_id": chat_id},
+        {
+            "$set": {
+                "couple": lovers
+            }
+        },
+        upsert=True
+    )
+
+
+# Karma functions
+
+
+async def get_karmas_count() -> dict:
+    chats = karmadb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return {}
+    chats_count = 0
+    karmas_count = 0
+    for chat in await chats.to_list(length=1000000):
+        for i in chat['karma']:
+            karmas_count += chat['karma'][i]['karma']
+        chats_count += 1
+    return {
+        "chats_count": chats_count,
+        "karmas_count": karmas_count
+    }
+
+
+async def get_karmas(chat_id: int) -> Dict[str, int]:
+    karma = karmadb.find_one({"chat_id": chat_id})
+    if karma:
+        karma = karma['karma']
+    else:
+        karma = {}
+    return karma
+
+
+async def get_karma(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name.lower().strip()
+    karmas = await get_karmas(chat_id)
+    if name in karmas:
+        return karmas[name]
+
+
+async def update_karma(chat_id: int, name: str, karma: dict):
+    name = name.lower().strip()
+    karmas = await get_karmas(chat_id)
+    karmas[name] = karma
+    karmadb.update_one(
+        {"chat_id": chat_id},
+        {
+            "$set": {
+                "karma": karmas
+            }
+        },
+        upsert=True
+    )
+
+
+# Alpha integer 
+
+async def int_to_alpha(user_id: int) -> str:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    text = ""
+    user_id = str(user_id)
+    for i in user_id:
+        text += alphabet[int(i)]
+    return text
+
+
+async def alpha_to_int(user_id_alphabet: str) -> int:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    user_id = ""
+    for i in user_id_alphabet:
+        index = alphabet.index(i)
+        user_id += str(index)
+    user_id = int(user_id)
+    return user_id
+
+
+
+# Karma On and OFF
+
+
+async def is_karma_on(chat_id: int) -> bool:
+    chat = karmadb.find_one({"chat_id_toggle": chat_id})
+    if not chat:
+        return True
+    return False
+
+
+async def karma_on(chat_id: int):
+    is_karma = await is_karma_on(chat_id)
+    if is_karma:
+        return
+    return karmadb.delete_one({"chat_id_toggle": chat_id})
+
+
+async def karma_off(chat_id: int):
+    is_karma = await is_karma_on(chat_id)
+    if not is_karma:
+        return
+    return karmadb.insert_one({"chat_id_toggle": chat_id})
+
+
+""" Gban functions """
+
+
+async def get_gbans_count() -> int:
+    users = gbansdb.find({"user_id": {"$gt": 0}})
+    users = await users.to_list(length=100000)
+    return len(users)
+
+
+async def is_gbanned_user(user_id: int) -> bool:
+    user = gbansdb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+
+async def add_gban_user(user_id: int):
+    is_gbanned = await is_gbanned_user(user_id)
+    if is_gbanned:
+        return
+    return gbansdb.insert_one({"user_id": user_id})
+
+
+async def remove_gban_user(user_id: int):
+    is_gbanned = await is_gbanned_user(user_id)
+    if not is_gbanned:
+        return
+    return gbansdb.delete_one({"user_id": user_id})
+
+
+
+""" Chats log functions """
+
+
+async def is_served_chat(chat_id: int) -> bool:
+    chat = chatsdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
+
+
+async def get_served_chats() -> list:
+    chats = chatsdb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return []
+    chats_list = []
+    for chat in await chats.to_list(length=1000000000):
+        chats_list.append(chat)
+    return chats_list
+
+
+async def add_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if is_served:
+        return
+    return chatsdb.insert_one({"chat_id": chat_id})
+
+
+async def remove_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if not is_served:
+        return
+    return chatsdb.delete_one({"chat_id": chat_id})
