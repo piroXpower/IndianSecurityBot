4383ce093db2ca23a5066cd6993232d067fa114c
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-15 15:21:33 +0530

    Update remote_cmds.py

--- TeamIndia/Plugins/remote_cmds.py
+++ TeamIndia/Plugins/remote_cmds.py
@@ -1,12 +1,12 @@
-from AliciaRobot import dispatcher
-from AliciaRobot.modules.helper_funcs.chat_status import (
+from TeamIndia import dispatcher
+from TeamIndia.Plugins.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from AliciaRobot.modules.helper_funcs.extraction import extract_user_and_text
-from AliciaRobot.modules.helper_funcs.filters import CustomFilters
+from TeamIndia.Plugins.helper_funcs.extraction import extract_user_and_text
+from TeamIndia.Plugins.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
