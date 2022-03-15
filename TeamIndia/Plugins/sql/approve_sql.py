6d68deffbf35099692f677da0c287792528bf94c
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-15 13:11:33 +0530

    Update approve_sql.py

--- TeamIndia/Plugins/sql/approve_sql.py
+++ TeamIndia/Plugins/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from AliciaRobot.modules.sql import BASE, SESSION
+from TeamIndia.Plugins.sql import BASE, SESSION
 
 class Approvals(BASE):
     __tablename__ = "approval"
