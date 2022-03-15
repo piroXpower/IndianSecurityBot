7ea5a2ae0a5226b0bcbbccac59bcf2d37c9d6da4
丂ﾑﾶﾶƳ <89745675+FriDayXD@users.noreply.github.com>
2022-03-15 11:26:44 +0530

    Update __telegraph.py

--- TeamIndia/Plugins/__telegraph.py
+++ TeamIndia/Plugins/__telegraph.py
@@ -1,12 +1,12 @@
-from AliciaRobot.events import register
-from AliciaRobot import telethn as tbot
+from TeamIndia.events import register
+from TeamIndia import telethn as tbot
 TMP_DOWNLOAD_DIRECTORY = "./"
 from telethon import events
 import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-himanshu = "ALICIA"
+blaze = "SAMMY"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=himanshu)
 auth_url = r["auth_url"]
