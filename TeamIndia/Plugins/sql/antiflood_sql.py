f998104433035e51539fefcf0a7b93d3e10759f9
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-15 13:11:12 +0530

    Update antiflood_sql.py

--- TeamIndia/Plugins/sql/antiflood_sql.py
+++ TeamIndia/Plugins/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, UnicodeText
 
-from AliciaRobot.modules.sql import SESSION, BASE
+from TeamIndia.Plugins.sql import SESSION, BASE
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
