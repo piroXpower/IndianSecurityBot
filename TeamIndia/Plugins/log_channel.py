42d7ff8209e0ae98bc9e0a6c898c5db1b75f20cf
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-15 11:07:40 +0530

    Update log_channel.py

--- TeamIndia/Plugins/log_channel.py
+++ TeamIndia/Plugins/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from AliciaRobot.modules.helper_funcs.misc import is_module_loaded
+from TeamIndia.Plugins.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
