b1aa7f9d7085f59f4a8c5fe002f125b85f0f076a
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-15 22:07:26 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop AliciaRobot
-net start AliciaRobot
+net stop TeamIndia
+net start TeamIndia
