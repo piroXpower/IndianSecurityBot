6a39dffdb30546acfe567e3a5a3cb44f022b82cd
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-15 13:14:12 +0530

    Update connection_sql.py

--- TeamIndia/Plugins/sql/connection_sql.py
+++ TeamIndia/Plugins/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 
-from AliciaRobot.modules.sql import SESSION, BASE
+from TeamIndia.Plugins.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
