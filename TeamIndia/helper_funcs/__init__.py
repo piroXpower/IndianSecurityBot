33d41d5cd9a046233b458efa2dcae9d52042fb20
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-15 17:59:03 +0530

    Update __init__.py

--- TeamIndia/helper_funcs/__init__.py
+++ TeamIndia/helper_funcs/__init__.py
@@ -1,1 +1,7 @@
-"""Helpers, also known as Utilities."""
+from TeamIndia import DEV_USERS, DRAGONS, DEMONS, TIGERS, CODERS, WOLVES, telethn
+
+IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS + CODERS
+
+IMMUNE_USERS = (
+    list(DRAGONS) + list(WOLVES) + list(DEMONS) + list(TIGERS) + list(DEV_USERS) + list(CODERS) 
+)
