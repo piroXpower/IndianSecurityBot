1c766ab2545a83a34638e351d9501826d93c0edb
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-15 13:19:55 +0530

    Update locks_sql.py

--- TeamIndia/Plugins/sql/locks_sql.py
+++ TeamIndia/Plugins/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from AliciaRobot.modules.sql import SESSION, BASE
+from TeamIndia.Plugins.sql import SESSION, BASE
 
 
 class Permissions(BASE):
