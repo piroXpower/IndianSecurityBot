f333d1e35a7e4b77f384026ffb9ad2f2b9eeef96
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-15 15:13:26 +0530

    Update shell.py

--- TeamIndia/Plugins/shell.py
+++ TeamIndia/Plugins/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from AliciaRobot import LOGGER, dispatcher
-from AliciaRobot.modules.helper_funcs.chat_status import dev_plus
+from TeamIndia import LOGGER, dispatcher
+from TeamIndia.Plugins.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
