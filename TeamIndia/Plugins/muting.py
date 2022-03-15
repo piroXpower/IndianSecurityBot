5917c866562c8db3287e0886c9d421b12809535d
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-15 15:28:38 +0530

    Update muting.py

--- TeamIndia/Plugins/muting.py
+++ TeamIndia/Plugins/muting.py
@@ -1,20 +1,20 @@
 import html
 from typing import Optional
 
-from AliciaRobot import LOGGER, TIGERS, dispatcher
-from AliciaRobot.modules.helper_funcs.chat_status import (
+from TeamIndia import LOGGER, TIGERS, dispatcher
+from TeamIndia.Plugins.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
     is_user_admin,
     user_admin,
 )
-from AliciaRobot.modules.helper_funcs.extraction import (
+from TeamIndia.Plugins.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from AliciaRobot.modules.helper_funcs.string_handling import extract_time
-from AliciaRobot.modules.log_channel import loggable
+from TeamIndia.Plugins.helper_funcs.string_handling import extract_time
+from TeamIndia.Plugins.log_channel import loggable
 from telegram import Bot, Chat, ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
