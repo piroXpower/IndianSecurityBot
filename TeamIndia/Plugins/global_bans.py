e7f728dd4423df5838a6cb813c7b451ee03b8f20
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-15 11:03:38 +0530

    Update global_bans.py

--- TeamIndia/Plugins/global_bans.py
+++ TeamIndia/Plugins/global_bans.py
@@ -2,18 +2,18 @@ import html
 import time
 from datetime import datetime
 from io import BytesIO
-from AliciaRobot.modules.sql.users_sql import get_user_com_chats
-import AliciaRobot.modules.sql.global_bans_sql as sql
-from AliciaRobot import (DEV_USERS, EVENT_LOGS, OWNER_ID, STRICT_GBAN, DRAGONS,
+from TeamIndia.Plugins.sql.users_sql import get_user_com_chats
+import TeamIndia.Plugins.sql.global_bans_sql as sql
+from TeamIndia import (DEV_USERS, EVENT_LOGS, OWNER_ID, STRICT_GBAN, DRAGONS,
                           SUPPORT_CHAT, SPAMWATCH_SUPPORT_CHAT, DEMONS, TIGERS,
                           WOLVES, sw, dispatcher)
-from AliciaRobot.modules.helper_funcs.chat_status import (is_user_admin,
+from TeamIndia.Plugins.helper_funcs.chat_status import (is_user_admin,
                                                            support_plus,
                                                            user_admin)
-from AliciaRobot.modules.helper_funcs.extraction import (extract_user,
+from TeamIndia.Plugins.helper_funcs.extraction import (extract_user,
                                                           extract_user_and_text)
-from AliciaRobot.modules.helper_funcs.misc import send_to_list
-from AliciaRobot.modules.sql.users_sql import get_all_chats
+from TeamIndia.Plugins.helper_funcs.misc import send_to_list
+from TeamIndia.Plugins.sql.users_sql import get_all_chats
 from telegram import ParseMode, Update
 from telegram.error import BadRequest, TelegramError
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
