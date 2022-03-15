2db3ae2737f42ad285acf60d169bb051e99a1891
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-15 13:10:28 +0530

    Update __init__.py

--- TeamIndia/Plugins/sql/__init__.py
+++ TeamIndia/Plugins/sql/__init__.py
@@ -1,4 +1,4 @@
-from AliciaRobot import DB_URI
+from TeamIndia import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
