4d5593568fdb35de60c642e96e666ab08f4fa4fc
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-15 13:11:56 +0530

    Update blacklist_sql.py

--- TeamIndia/Plugins/sql/blacklist_sql.py
+++ TeamIndia/Plugins/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from AliciaRobot.modules.sql import SESSION, BASE
+from TeamIndia.Plugins.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
