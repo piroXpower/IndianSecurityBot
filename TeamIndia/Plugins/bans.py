44f3598b4561d95d650e01d29a27d82c70ef5942
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-16 15:14:15 +0530

    Update bans.py

--- TeamIndia/Plugins/bans.py
+++ TeamIndia/Plugins/bans.py
@@ -116,7 +116,7 @@ def ban(update: Update, context: CallbackContext) -> str:
         log += "<b>Reason:</b> {}".format(reason)
 
     try:
-        chat.ban_member(user_id)
+        chat.kick_member(user_id)
 
         if silent:
             if message.reply_to_message:
