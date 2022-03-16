09f825766e4e6db280ad1f53eb1265197cd435ff
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-16 08:28:58 +0530

    Update __telegraph.py

--- TeamIndia/Plugins/__telegraph.py
+++ TeamIndia/Plugins/__telegraph.py
@@ -1,3 +1,4 @@
+
 from TeamIndia.events import register
 from TeamIndia import telethn as tbot
 TMP_DOWNLOAD_DIRECTORY = "./"
@@ -6,13 +7,13 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-blaze = "SAMMY"
+Sammy = "Innexia"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=himanshu)
+r = telegraph.create_account(short_name=Sammy)
 auth_url = r["auth_url"]
 
 
-@register(pattern="^/t(m|t) ?(.*)")
+@register(pattern="^/t(m|xt) ?(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -41,8 +42,8 @@ async def _(event):
                 end = datetime.now()
                 ms_two = (end - start).seconds
                 os.remove(downloaded_file_name)
-                await h.edit("Uploaded to https://telegra.ph{}".format(media_urls[0]), link_preview=True)
-        elif input_str == "t":
+                await h.edit("Uploaded to https://telegra.ph{})".format(media_urls[0]), link_preview=True)
+        elif input_str == "xt":
             user_object = await tbot.get_entity(r_message.sender_id)
             title_of_page = user_object.first_name # + " " + user_object.last_name
             # apparently, all Users do not have last_name field
@@ -77,3 +78,4 @@ async def _(event):
 def resize_image(image):
     im = Image.open(image)
     im.save(image, "PNG")
+
