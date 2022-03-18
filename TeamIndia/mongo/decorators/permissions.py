57c8fbe984816458aec78c647ca39f7acda57ae4
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-03-18 20:05:21 +0530

    Update permissions.py

--- TeamIndia/mongo/decorators/permissions.py
+++ TeamIndia/mongo/decorators/permissions.py
@@ -2,7 +2,7 @@ from functools import wraps
 from traceback import format_exc as err
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from pyrogram.types import Message
-from Rose import SUDO_USERS as SUDOERS, pbot as app
+from TeamIndia import SUDO_USERS as SUDOERS, pbot as app
 
 
 async def member_permissions(chat_id: int, user_id: int):
